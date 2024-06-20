ac10859b4776ab525ee7e1d135ebec3f3226b1e3
Akshansh Jain <akshansh.jain@snapdeal.com>
2024-06-20 14:46:19 +0530

    updated requirements

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'jnakshansh',
 ]
 
@@ -126,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
